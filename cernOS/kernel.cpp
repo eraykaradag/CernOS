fb7cbe12f6369a642d7000ffaea547f97973043c
eraykaradag <94753359+eraykaradag@users.noreply.github.com>
2023-08-03 12:19:10 +0300

    :)

---
+++ cernOS/kernel.cpp
@@ -0,0 +1,12 @@
+void printf(char* str){
+	unsigned short* VideoMemory = (unsigned short*) 0xb8000;
+
+	for (int i = 0; str[i] != '\0'; i++){
+		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+	}
+}
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnum){
+	
+	printf("cernOS ilk gosterim");
+	while(1);
+}
